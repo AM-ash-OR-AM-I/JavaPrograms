dd04494673ce8b9cc007ae079189fff34396ee7e
Ashutosh <ashutosh2gudu@gmail.com>
2022-07-05 09:40:05 +0530

    Updated files

--- src/StackQueueLinkedList.java
+++ src/StackQueueLinkedList.java
@@ -1,23 +1,59 @@
-interface StackInterface<T>{
-    void push(T data);
+import java.util.Scanner;
+
+interface StackInterface{
+    String MENU = """
+                ** STACK MENU **
+                1. PUSH
+                2. POP
+                3. DISPLAY
+                Any other number to show menu.
+                """;
+    Scanner sc = new Scanner(System.in);
+    void push(String data);
     void pop();
-    void displayStack(String msg);
+    void displayStack();
+    default void menu(){
+        System.out.println(MENU);
+        int choice = 1;
+        while(choice!=0){
+            switch (choice){
+                case 1 -> {
+                    System.out.print("Enter the number: ");
+                    String elem = sc.next();
+                    push(elem);
+                }
+                case 2 -> pop();
+                case 3 -> displayStack();
+                default -> System.out.println(MENU);
+
+            }
+            System.out.print("\nEnter your choice: ");
+            choice = sc.nextInt();
+        }
+
+    }
 }
 
-class Stack<T extends Comparable<T>> extends LinkedList<T> implements StackInterface<T>{
+interface QueueInterface{
+    void enqueue(String data);
+    void dequeue();
+    void displayQueue();
+}
+
+class Stack extends LinkedList<String> implements StackInterface{
     int MAX_LENGTH;
     int length;
     Stack(int n){
         MAX_LENGTH = n;
+        menu();
     }
-    public void push(T data) {
+    public void push(String data) {
         if (length < MAX_LENGTH){
             length++;
             insertStart(data);
-            displayStack(data+" is pushed.");
         }
         else {
-            System.out.println("Overflow: Stack is already full.");
+            System.out.println("\nOverflow: Stack is already full.");
         }
     }
 
@@ -30,9 +66,9 @@ class Stack<T extends Comparable<T>> extends LinkedList<T> implements StackInter
             System.out.println("Underflow: Stack is Empty");
         }
     }
-    public void displayStack(String msg){
+    public void displayStack(){
         Node current = start;
-        System.out.println("\nSTACK after "+msg);
+        System.out.println("\n::STACK::");
         while (current!=null){
             if (current==start)
                 System.out.printf("| %5s | --> TOP%n",current.data);
@@ -43,14 +79,52 @@ class Stack<T extends Comparable<T>> extends LinkedList<T> implements StackInter
     }
 }
 
+
+class Queue extends LinkedList<String> implements QueueInterface{
+
+    @Override
+    public void enqueue(String data) {
+        insertEnd(data);
+    }
+
+    @Override
+    public void dequeue() {
+        if (start!=end){
+            System.out.println("\nElement removed = "+start.data);
+            start = start.next;
+        }
+        else {
+            System.out.println("Queue is empty.");
+        }
+    }
+
+    @Override
+    public void displayQueue() {
+        Node current = start;
+        System.out.println("\n::QUEUE::");
+        while (current!=null){
+            if (start==end)
+                System.out.printf("| %5s | --> FRONT & REAR %n",current.data);
+            else if (current==start)
+                System.out.printf("| %5s | --> FRONT%n",current.data);
+            else if (current.next==null)
+                System.out.printf("| %5s | --> REAR%n",current.data);
+            else
+                System.out.printf("| %5s |%n",current.data);
+            current = current.next;
+        }
+    }
+}
+
 public class StackQueueLinkedList {
     public static void main(String[] args) {
-        StackInterface<Integer> stack = new Stack<>(3);
-        stack.push(24);
-        stack.push(35);
-        stack.push(64);
-        stack.push(52343);
-//        stack.pop();
-//        stack.displayStack(52343+" is removed");
+//        StackInterface stack = new Stack(3); // Gives access to only methods of stack.
+        QueueInterface queue = new Queue();
+        queue.enqueue("234");
+        queue.displayQueue();
+        queue.enqueue("53");
+        queue.displayQueue();
+        queue.dequeue();
+        queue.displayQueue();
     }
 }
